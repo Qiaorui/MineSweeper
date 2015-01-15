254c08eeb10dbd6ddc96d394549df03489a0dfa5
Ignacio N. Lucero Ascencio <igwisnirrol@gmail.com>
2015-01-15 17:05:31 +0100

    Comenta las clases en domainLayer.serviceLocator

--- src/com/mineSweeper/domainLayer/serviceLocator/ServeiLocator.java
+++ src/com/mineSweeper/domainLayer/serviceLocator/ServeiLocator.java
@@ -11,10 +11,18 @@ public class ServeiLocator {
 
     private static volatile ServeiLocator instance;
 
+    /**
+     * ServeiLocator
+     * Crea una instancia del localizador de servicios
+     */
     private ServeiLocator() {
 
     }
 
+    /**
+     * getInstance
+     * @return La instancia del localizador de servicios
+     */
     public static ServeiLocator getInstance() {
         if (instance == null) {
             synchronized (ServeiLocator.class) {
@@ -26,6 +34,11 @@ public class ServeiLocator {
         return instance;
     }
 
+    /**
+     * obtenirServei
+     * @param servei El nombre del servicio que se quiere localizar
+     * @return Una instancia del servicio que se quiere localizar, si existe y se encuentra.
+     */
     public SvEmail obtenirServei(String servei) {
         if (servei == "SvEmail") return new SvEmail();
         else return null;
